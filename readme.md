01c57c5aa388f6bf5e2d3ce3009a6df2f2a61e82
Andreas Ellingsen <andreas.ellingsen@knowit.se>
2022-06-17 07:55:39 +0200

    added some kind of scene, did not manage to resize pic to use as scene tiles

--- readme.md
+++ readme.md
@@ -1,6 +1,5 @@
 ## Should probably add these things
 * Read a textfile that describes the world as tiles, render it (one character per tile)
-* Simple audio
 * Jumping and gravity
 * Collision detection (so gravity doesn't just pull you off the screen)
 * Maybe another entity that's moving around/patrolling
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+extern crate csv;
+
 use macroquad::{prelude::*};
 use soloud::*;
-use std::vec::Vec;
+use std::{vec::Vec};
 
 fn check_add_point(points: &mut Vec<Vec2>) {
     if is_mouse_button_released(MouseButton::Left) {
@@ -25,18 +27,47 @@ fn move_player_character(mut x: f32, mut y:  f32) -> (f32, f32) {
     return (x, y);
 }
 
+fn read_map() -> Vec<Vec<String>> {
+    let mut rdr = csv::Reader::from_path("assets/scene.csv").unwrap();
+    let mut map = vec![vec![String::from(""); 50]; 50];
+    for (j, record) in rdr.records().enumerate() {
+        for (i, tile) in record.unwrap().iter().enumerate() {
+            map[i][j] = tile.to_string();
+        }
+    }
+    return map;
+}
+
+fn draw_map(map: Vec<Vec<String>>) {
+    let squares = 50 as f32;
+    let square_size_width = screen_width() / squares;
+    let square_size_height = screen_height() / squares;
+
+    for i in 0..50 {
+        for j in 0..50 {
+            match map[j][i].as_str(){
+                "T"=>draw_rectangle(j as f32*square_size_width, i as f32*square_size_height, square_size_width,square_size_height,BLUE),
+                "G"=>draw_rectangle(j as f32*square_size_width, i as f32*square_size_height, square_size_width,square_size_height,GREEN),
+                "A"=>draw_rectangle(j as f32*square_size_width, i as f32*square_size_height, square_size_width,square_size_height,LIGHTGRAY),
+                _=>print!(""),
+            }
+        }
+    }
+}
 
 #[macroquad::main("BasicShapes")]
 async fn main() {
-    let texture = load_texture("assets/grigor.png").await.unwrap();
+    let texture = load_texture("assets/pictures/grigor.png").await.unwrap();
     let background_sound = Soloud::default().unwrap();  
     let mut wav_background = audio::Wav::default();
     let mut x = 50.0;
     let mut y = 50.0;
     let mut clicked_points = Vec::new();
-    wav_background.load(&std::path::Path::new("assets/monkey-music.mp3")).unwrap();
+    wav_background.load(&std::path::Path::new("assets/music/monkey-music.mp3")).unwrap();
     background_sound.play(&wav_background);
 
+    let map = read_map();
+    
     loop {
         clear_background(LIGHTGRAY);
         
@@ -52,6 +83,8 @@ async fn main() {
         for point in &clicked_points {
             draw_circle(point.x, point.y, 10.0, RED);
         }
+
+        draw_map(map.clone());
 
         draw_texture(texture, x, y, WHITE);
         next_frame().await
